a20fdf15ca380a85bb2cd2f4c55decfaa03e16b8
zxkeyy <z_selmani@estin.dz>
2024-03-17 15:17:25 +0100

    Created Panel 7

---
+++ src/pages/Homepage/Components/Panel7.tsx
@@ -0,0 +1,27 @@
+import { Box, Text } from "@chakra-ui/react";
+import ActionButton from "../../../components/ActionButton";
+import Image1 from "../../../assets/Panel7Image1.png";
+
+const Panel7 = () => {
+  return (
+    <Box
+      aspectRatio={1920 / 315}
+      width={"100%"}
+      bgImage={Image1}
+      bgSize={"100%"}
+      display={"flex"}
+      justifyContent={"space-evenly"}
+      alignItems={"center"}
+      paddingX={"400px"}
+    >
+      <Text color={"white"} maxWidth={"50%"} fontSize={"40px"}>
+        Delivering the <b>best security solutions</b> to our clients
+      </Text>
+      <ActionButton fontSize={"12px"} height={"18%"} width={"20%"}>
+        MAKE AN APPOINTMENT
+      </ActionButton>
+    </Box>
+  );
+};
+
+export default Panel7;
