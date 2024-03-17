ace2a510ab3008a41f911d23c5b5b031d5ca66c8
zxkeyy <z_selmani@estin.dz>
2024-03-17 14:27:47 +0100

    created panel 5

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -5,6 +5,8 @@ import Hero from "./Components/Hero";
 import Panel1 from "./Components/Panel1";
 import Panel2 from "./Components/Panel2";
 import Panel3 from "./Components/Panel3";
+import Panel4 from "./Components/Panel4";
+import Panel5 from "./Components/Panel5";
 
 const HomePage = () => {
   return (
@@ -13,6 +15,8 @@ const HomePage = () => {
       <Panel1 />
       <Panel2 />
       <Panel3 />
+      <Panel4 />
+      <Panel5 />
     </Box>
   );
 };
