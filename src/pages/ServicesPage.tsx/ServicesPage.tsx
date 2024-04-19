1f1eda26485ce1c93318c357fd3f6312ab9542c9
zxkeyy <z_selmani@estin.dz>
2024-04-19 17:33:33 +0200

    Addes services page

---
+++ src/pages/ServicesPage.tsx/ServicesPage.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import Panel1 from "./Components/Panel1";
+
+const ServicesPage = () => {
+  return (
+    <Box bgColor={"bg.500"}>
+      <Panel1/>
+    </Box>
+  );
+};
+
+export default ServicesPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,13 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Homepage/Homepage";
 import Layout from "./pages/Layout";
 import NotFoundPage from "./pages/NotFoundPage.tsx/NotFoundPage";
+import ServicesPage from "./pages/ServicesPage.tsx/ServicesPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    errorElement: <NotFoundPage/>,
-    children: [{ index: true, element: <HomePage /> }],
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/services", element: <ServicesPage /> },
+    ],
   },
 ]);
 
